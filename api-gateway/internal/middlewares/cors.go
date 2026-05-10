67ebea880e1e9e6b4a3bf28e7cd43787d2035a61
Hristo Partenov <hristokubev@gmail.com>
2026-05-11 00:42:27 +0300

    add cors

---
+++ api-gateway/internal/middlewares/cors.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"github.com/gorilla/handlers"
+	"net/http"
+)
+
+func CORS(next http.Handler, allowedOrigins []string) http.Handler {
+	return handlers.CORS(
+		handlers.AllowedOrigins(allowedOrigins),
+		handlers.AllowedMethods([]string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodDelete,
+			http.MethodOptions,
+		}),
+		handlers.AllowedHeaders([]string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+			InternalTokenHeader,
+		}),
+		handlers.OptionStatusCode(http.StatusNoContent),
+	)(next)
+}
